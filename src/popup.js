3766190d94cec584ccd0d77811118231f73d14c0
Joshua Tanner <jtanner2@pacbell.net>
2013-08-24 19:31:25 -0700

    wip

--- src/popup.js
+++ src/popup.js
@@ -3,20 +3,21 @@
 var KANJI_MAX = 0x9faf
 var KANJI_MIN = 0x4e00
 
+//kanji are mapped to ranges because chrome's total sync storage max is substantially > than its per item max
 //Comments explain how the below were computed, but if they do change, changing the constants with them
-//would break our storage. So the constants are constant for now.
+//would break our storage. So the constants are constant for now. 
 var STORAGE_ENTRIES = 25 //chrome.storage.sync.QUOTA_BYTES / chrome.storage.sync.QUOTA_BYTES_PER_ITEM
-var HASH_RANGE = 837 //Math.ceil((KANJI_MAX - KANJI_MIN) / STORAGE_ENTRIES)
+var HASH_RANGE = 837 //Math.floor((KANJI_MAX - KANJI_MIN) / STORAGE_ENTRIES)+1
 
-function syncHashStructure() //demo function, no actual use
-{
-  var syncHash = {}
-  for (var i = 1; i <= STORAGE_ENTRIES; i++)
-  {
-    syncHash[i] = ( KANJI_MIN+((i-1)*HASH_RANGE) )+"-"+( KANJI_MIN+(i*HASH_RANGE) )
-  }
-  return syncHash
-}
+
+// kanji are thrown into ranges based on the formula: Math.floor((kanji.charCodeAt(0) - KANJI_MIN)/HASH_RANGE)+1
+//ceiling is out of the question because then the lowest value kanji tries to put itself in nonexistant range 0
+//so we use floor+1, but unless we know our top range to exceed our max, a max value kanji could produce a 
+//nonexistant range 26 by dividing exactly, producing floor(25)=25 and then +1 gives us 26. So floor+1 for HASH_RANGE
+//ensures our top range is always slightly above max, although it usually is anyway. The top range will catch 
+//slightly fewer kanji, but that's no real concern
+
+
 
 function saveSynced(kanjiHash)
 {
@@ -30,16 +31,45 @@ function saveSynced(kanjiHash)
   for (var kanji in kanjiHash)
   {
     //have to use floor+1, or numbers that == the bottom of the range break it
-    //this works, even for the highest and lowest kanji values (maybe because our top range slightly
-    //exceeds max, or maybe because it just works. Not too concerned either way)
+    //this works, even for the highest and lowest kanji values (likely because our top range slightly
+    //exceeds max so division for the highest number still produces a number <25)
     syncHash[Math.floor((kanji.charCodeAt(0) - KANJI_MIN)/HASH_RANGE)+1][kanji] = kanjiHash[kanji]
   }
+
+  chrome.storage.sync.set(syncHash, function(){
+    if (chrome.runtime.lastError)
+    {
+       error(chrome.runtime.lastError.message)
+    }
+  })
 
   return syncHash
 }
 
-function loadSynced(kanjiHash)
+function loadSynced(callback)
 {
+  var loadList = []
+  for (var i = 1; i <= STORAGE_ENTRIES; i++)
+  {
+    loadList.push(i)
+  }
+  chrome.storage.sync.get(loadList, function(loaded)
+  {
+    if (chrome.runtime.lastError)
+    {
+       error(chrome.runtime.lastError.message)
+       return
+    }
+    var syncHash = {}
+    for (var loadItem in loadList)
+    {
+      for (var item in loadItem)
+      {
+        syncHash[item] = loadItem[item]
+      }
+    }
+    callback(syncHash)
+  })
 
 }
 
@@ -174,4 +204,9 @@ function draw(hash)
   {
     div = $('#noKanji')
   }
+}
+
+function error(errorText)
+{
+  $('#text').text(errorText)
 }
